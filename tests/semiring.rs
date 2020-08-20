3c05f9f0cd3e4f8056a40039826f0074ba15ea55
Remy Wang <remy.sucre@gmail.com>
2020-08-20 16:34:31 -0700

    Prove running total for real.

--- tests/semiring.rs
+++ tests/semiring.rs
@@ -172,76 +172,64 @@ fn rules() -> Vec<Rewrite<Lambda, BindAnalysis>> {
     rs.extend(vec![
         rw!("id-51"; "(* (I (< ?j ?t)) (I (<= ?j ?t)))" => "(I (< ?j ?t))"),
         rw!("id-52"; "(* (I (= ?j ?t)) (I (<= ?j ?t)))" => "(I (= ?j ?t))"),
+        rw!("id-53"; "(I (< ?j ?t))" => "(I (<= ?j (- ?t (lit 1))))"),
     ]);
     rs.extend(vec![
-        rw!("S-def-APSP"; "(sum ?w1 (* (rel R ?x ?y ?w1) ?w1))" => "(rel S ?x ?y)"),
+        //rw!("S-def-APSP"; "(sum ?w1 (* (rel R ?x ?y ?w1) ?w1))" => "(rel S ?x ?y)"),
         rw!("S-def-running-total"; "(sum ?j (sum ?w (* (* (rel R ?t ?j ?w) ?w) (* (I (<= (lit 1) ?j)) (I (<= ?j ?t))))))" => "
 (rel S ?t)"),
     ]);
     rs
 }
 
-egg::test_fn! {
-    apsp, rules(),
-    runner = Runner::default()
-        .with_time_limit(std::time::Duration::from_secs(20))
-        .with_node_limit(100_000)
-        .with_iter_limit(60),
-    "(sum (var w)
-        (* (+ (rel E (var x) (var y) (var w))
-              (sum (var y)
-                 (sum (var w1)
-                    (sum (var w2)
-                       (* (rel R (var x) (var y) (var w1))
-                          (* (rel E (var y) (var z) (var w2))
-                             (I (= (* (var w1) (var w2)) (var w)))))))))
-           (var w)))"
-    =>
-    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) w))
-    //    (sum (var w)
-    //       (sum (var y)
-    //          (sum (var w1)
-    //              (sum (var w2)
-    //                   (* w (* (rel R (var x) (var y) (var w1))
-    //                      (* (rel E (var y) (var z) (var w2))
-    //                         (I (= (* w1 w2) w))))))))))"
-    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
-    //    (sum (var y) (sum (var w1) (sum (var w2)
-    //      (* (rel R (var x) (var y) (var w1))
-    //         (* (rel E (var y) (var z) (var w2))
-    //            (sum (var w) (* (I (= (* (var w1) (var w2)) (var w))) (var w)))))))))"
-    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
-    //    (sum (var y) (sum (var w1) (sum (var w2)
-    //      (* (rel R (var x) (var y) (var w1))
-    //         (* (rel E (var y) (var z) (var w2))
-    //            (* (var w1) (var w2))))))))"
-    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
-    //    (sum (var y) (sum (var w1) (sum (var w2)
-    //      (* (* (rel R (var x) (var y) (var w1))
-    //            (var w1))
-    //         (* (rel E (var y) (var z) (var w2))
-    //            (var w2)))))))"
-    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
-    //    (sum (var y)
-    //       (* (sum (var w1) (* (rel R (var x) (var y) (var w1)) (var w1)))
-    //          (sum (var w2) (* (rel E (var y) (var z) (var w2)) (var w2))))))"
-    "(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
-        (sum (var y)
-           (* (rel S (var x) (var y))
-              (sum (var w2) (* (rel E (var y) (var z) (var w2)) (var w2))))))"
-    ,
-}
-
 //egg::test_fn! {
-//    inequality, rules(),
-//    "(* (* (rel v (var j) (var w)) (I (= (var t) (var j))))
-//              (* (var w)
-//                 (* (I (<= (lit 1) (var j)))
-//                    (I (<= (var j) (var t))))))"
+//    apsp, rules(),
+//    runner = Runner::default()
+//        .with_time_limit(std::time::Duration::from_secs(20))
+//        .with_node_limit(100_000)
+//        .with_iter_limit(60),
+//    "(sum (var w)
+//        (* (+ (rel E (var x) (var y) (var w))
+//              (sum (var y)
+//                 (sum (var w1)
+//                    (sum (var w2)
+//                       (* (rel R (var x) (var y) (var w1))
+//                          (* (rel E (var y) (var z) (var w2))
+//                             (I (= (* (var w1) (var w2)) (var w)))))))))
+//           (var w)))"
 //    =>
-//    "(* (* (rel v (var j) (var w)) (I (= (var t) (var j))))
-//              (* (var w)
-//                 (I (<= (lit 1) (var j)))))"
+//    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) w))
+//    //    (sum (var w)
+//    //       (sum (var y)
+//    //          (sum (var w1)
+//    //              (sum (var w2)
+//    //                   (* w (* (rel R (var x) (var y) (var w1))
+//    //                      (* (rel E (var y) (var z) (var w2))
+//    //                         (I (= (* w1 w2) w))))))))))"
+//    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
+//    //    (sum (var y) (sum (var w1) (sum (var w2)
+//    //      (* (rel R (var x) (var y) (var w1))
+//    //         (* (rel E (var y) (var z) (var w2))
+//    //            (sum (var w) (* (I (= (* (var w1) (var w2)) (var w))) (var w)))))))))"
+//    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
+//    //    (sum (var y) (sum (var w1) (sum (var w2)
+//    //      (* (rel R (var x) (var y) (var w1))
+//    //         (* (rel E (var y) (var z) (var w2))
+//    //            (* (var w1) (var w2))))))))"
+//    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
+//    //    (sum (var y) (sum (var w1) (sum (var w2)
+//    //      (* (* (rel R (var x) (var y) (var w1))
+//    //            (var w1))
+//    //         (* (rel E (var y) (var z) (var w2))
+//    //            (var w2)))))))"
+//    //"(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
+//    //    (sum (var y)
+//    //       (* (sum (var w1) (* (rel R (var x) (var y) (var w1)) (var w1)))
+//    //          (sum (var w2) (* (rel E (var y) (var z) (var w2)) (var w2))))))"
+//    "(+ (sum (var w) (* (rel E (var x) (var y) (var w)) (var w)))
+//        (sum (var y)
+//           (* (rel S (var x) (var y))
+//              (sum (var w2) (* (rel E (var y) (var z) (var w2)) (var w2))))))"
 //    ,
 //}
 
@@ -269,13 +257,25 @@ egg::test_fn! {
     //          (* (var w)
     //             (* (I (<= (lit 1) (var j)))
     //                (I (<= (var j) (var t)))))))))"
+    //"(+ (sum (var j) (sum (var w)
+    //       (* (* (rel v (var j) (var w)) (I (= (var j) (var t))))
+    //          (* (var w) (I (<= (lit 1) (var j)))))))
+    //    (sum (var j) (sum (var w)
+    //       (* (* (rel R (- (var t) (lit 1)) (var j) (var w))
+    //             (* (I (< (var j) (var t)))
+    //                (I (> (var t) (lit 1)))))
+    //          (* (var w) (I (<= (lit 1) (var j))))))))"
+    //"(+ (sum (var j) (sum (var w)
+    //       (* (* (rel v (var j) (var w)) (I (= (var j) (var t))))
+    //          (* (var w) (I (<= (lit 1) (var j)))))))
+    //    (* (I (> (var t) (lit 1)))
+    //    (sum (var j) (sum (var w)
+    //       (* (* (rel R (- (var t) (lit 1)) (var j) (var w))
+    //             (I (<= (var j) (- (var t) (lit 1)))))
+    //          (* (var w) (I (<= (lit 1) (var j)))))))))"
     "(+ (sum (var j) (sum (var w)
            (* (* (rel v (var j) (var w)) (I (= (var j) (var t))))
               (* (var w) (I (<= (lit 1) (var j)))))))
-        (sum (var j) (sum (var w)
-           (* (* (rel R (- (var t) (lit 1)) (var j) (var w))
-                 (* (I (< (var j) (var t)))
-                    (I (> (var t) (lit 1)))))
-              (* (var w) (I (<= (lit 1) (var j))))))))"
+        (* (I (> (var t) (lit 1))) (rel S (- (var ?t) (lit 1)))))"
     ,
 }
