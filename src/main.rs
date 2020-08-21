c0a00a320e6f8d1a2364b098d1dfbc941abec3f1
Remy Wang <remy.sucre@gmail.com>
2020-08-21 09:03:10 -0700

    Compare roots instead.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ fn main() {
              (* (rel R (- (var t) (lit 1)) (var j) (var w))
                 (* (I (< (var j) (var t))) (I (> (var t) (lit 1))))))
           (* (var w) (* (I (<= (lit 1) (var j))) (I (<= (var j) (var t))))))))".parse().unwrap())
-    .with_expr(&"(rel S (- (var ?t) (lit 1)))".parse().unwrap())
+    .with_expr(&"(+ (sum (var j) (sum (var w)
+           (* (* (rel v (var j) (var w)) (I (= (var j) (var t))))
+              (* (var w) (I (<= (lit 1) (var j)))))))
+        (* (I (> (var t) (lit 1))) (rel S (- (var ?t) (lit 1)))))".parse().unwrap())
     .with_hook(|runner| {
         let eg = &runner.egraph;
-        if eg[runner.roots[1]].len() > 1 {
-            Err("proved eq".to_string())
+        if eg.find(runner.roots[0]) == eg.find(runner.roots[1]) {
+            Err("proven eq".to_string())
         } else {
             Ok(())
         }
