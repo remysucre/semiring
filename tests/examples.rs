5d110c5d1923cbe8ccefddbc66153ce3139c8684
Remy Wang <remy.sucre@gmail.com>
2020-11-09 15:25:27 -0800

    Verify sliding window.

--- tests/examples.rs
+++ tests/examples.rs
@@ -3,6 +3,8 @@ use semiring::rewrites::*;
 
 fn check_eq(x: &str, y:&str) {
     let runner = Runner::default()
+        .with_iter_limit(60)
+        .with_node_limit(50_000)
         .with_expr(&x.parse().unwrap())
         .with_expr(&y.parse().unwrap())
         .run(&rules());
@@ -63,5 +65,57 @@ fn running_total() {
                    (var w))
                 (* (I (= (var t) (var j)))
                    (I (<= 1 (var j))))))))"
+    )
+}
+
+#[test]
+fn sliding_window() {
+    check_eq("
+(- (sum (var w)
+     (sum (var j)
+          (* (* (var w) (* (I (<= 1 (var j)))
+                           (I (<= (var j) (var t)))))
+             (+ (* (I (= (var t) (var j)))
+                   (rel v (var j) (var w)))
+                (* (rel R (- (var t) 1) (var j) (var w))
+                   (* (I (< (var j) (var t)))
+                      (I (> (var t) 1))))))))
+   (sum (var w)
+     (sum (var j)
+          (* (* (var w) (* (I (<= 1 (var j)))
+                           (I (<= (var j) (- (var t) (var k))))))
+             (+ (* (I (= (- (var t) (var k)) (var j)))
+                   (rel v (var j) (var w)))
+                (* (rel R (- (- (var t) (var k)) 1) (var j) (var w))
+                   (* (I (< (var j) (- (var t) (var k))))
+                      (I (> (- (var t) (var k)) 1)))))))))
+", "
+(+ (- (* (I (> (var t) 1))
+         (sum (var j)
+              (sum (var w)
+                   (* (* (rel R (- (var t) 1) (var j) (var w))
+                         (var w))
+                      (* (I (<= (var j) (- (var t) 1)))
+                         (I (<= 1 (var j))))))))
+      (* (I (> (- (var t) (var k)) 1))
+         (sum (var j)
+              (sum (var w)
+                   (* (* (rel R (- (- (var t) (var k)) 1) (var j) (var w))
+                         (var w))
+                      (* (I (<= (var j) (- (- (var t) (var k)) 1)))
+                         (I (<= 1 (var j)))))))))
+   (- (sum (var j)
+           (sum (var w)
+                (* (* (rel v (var j) (var w))
+                      (var w))
+                   (* (I (= (var t) (var j)))
+                      (I (<= 1 (var j)))))))
+      (sum (var j)
+           (sum (var w)
+                (* (* (rel v (var j) (var w))
+                      (var w))
+                   (* (I (= (- (var t) (var k)) (var j)))
+                      (I (<= 1 (var j)))))))))
+"
     )
 }
