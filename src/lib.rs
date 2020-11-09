e92ebaa191725e36fde76667a3a39f4e6e611ad1
Remy Wang <remy.sucre@gmail.com>
2020-11-09 08:51:26 -0800

    Cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod analysis;
 pub mod lang;
 pub mod rewrites;
+pub mod smt;
 
 pub type EGraph = egg::EGraph<lang::Semiring, analysis::SemiringAnalysis>;
--- src/rewrites.rs
+++ src/rewrites.rs
@@ -1,5 +1,4 @@
 use egg::{rewrite as rw, *};
-use std::collections::HashMap;
 
 use crate::analysis::*;
 use crate::lang::*;
@@ -264,17 +263,6 @@ fn rw_1(
     .unwrap()
 }
 
-pub fn lemmas() -> Vec<Rewrite<Semiring, SemiringAnalysis>> {
-    vec![
-        rw!("l-49";  "(I (< ?j ?t))" <=> "(+ (I (< ?j (- ?t 1))) (I (= ?j (- ?t 1))))"),
-        // rw!("l-50";  "0" <=> "(* (I (< ?j ?s)) (I (= ?j ?s)))"),
-        rw!("l-51";  "(I (< ?j ?t))" <=> "(* (I (< ?j ?t)) (I (<= ?j ?t)))"),
-        rw!("l-52";  "(I (= ?j ?t))" <=> "(* (I (= ?j ?t)) (I (<= ?j ?t)))"),
-        rw!("l-53";  "(I (< ?j ?t))" <=> "(I (<= ?j (- ?t 1)))"),
-    ]
-    .concat()
-}
-// REVIEW
 pub fn normalize() -> Vec<Rewrite<Semiring, SemiringAnalysis>> {
     vec![
         rw_1(
@@ -303,9 +291,9 @@ pub fn normalize() -> Vec<Rewrite<Semiring, SemiringAnalysis>> {
             }}}
             if free(var("?x"), var("?b"))),
         // rw_1("let-const", "(let ?v1 ?e ?n))", "?n" if is_const(var("?c"))),
-        rw!("let-const"; "(let ?v1 ?e ?n))" => "?n" if is_const(var("?c"))),
-        rw_1("let-var-same", "(let ?v1 ?e (var ?v1))", "?e"),
-        rw!("let-var-diff"; "(let ?v1 ?e (var ?v2))" =>
+        rw!("let-const"; "(let ?v1 ?e ?n))" => "?n" if is_const(var("?n"))),
+        rw_1("let-var-same", "(let ?v1 ?e ?v1)", "?e"),
+        rw!("let-var-diff"; "(let (var ?v1) ?e (var ?v2))" =>
             { Destroy { e: "(var ?v2)".parse::<Pattern<Semiring>>().unwrap() }}
             if is_not_same_var(var("?v1"), var("?v2"))),
         rw_1(
@@ -335,51 +323,13 @@ pub fn normalize() -> Vec<Rewrite<Semiring, SemiringAnalysis>> {
     ]
 }
 
-// TODO use iteration data to compute this incrementally
-pub fn solve_eqs(runner: &mut Runner<Semiring, SemiringAnalysis>) -> Result<(), String> {
-    let mut fingerprints: HashMap<&Vec<i32>, Vec<Id>> = HashMap::new();
-    for class in runner.egraph.classes() {
-        if let Some(fp) = &class.data.fingerprint {
-            fingerprints.entry(fp).or_insert(vec![]).push(class.id);
-        }
-    }
-    let mut to_union = vec![];
-    for matches in fingerprints.values() {
-        for c_1 in matches.iter() {
-            for c_2 in matches.iter() {
-                let mut extractor = Extractor::new(&runner.egraph, AstSize);
-                let (_, e_1) = extractor.find_best(*c_1);
-                let (_, e_2) = extractor.find_best(*c_2);
-                let local_runner = Runner::default()
-                    .with_expr(&e_1)
-                    .with_expr(&e_2)
-                    .run(&lemmas());
-                if local_runner.egraph.find(local_runner.roots[0])
-                    == local_runner.egraph.find(local_runner.roots[1])
-                {
-                    to_union.push((*c_1, *c_2));
-                }
-            }
-        }
-    }
-    for (c_1, c_2) in to_union {
-        runner.egraph.union(c_1, c_2);
-    }
-    Ok(())
-}
-
-pub fn gen_rosette(lhs: &str, rhs: &str, fvs: &Vec<Id>) -> String {
-    // FIXME this won't work, really needs to get the vars
-    let fvs_s: Vec<String> = fvs.iter().map(|id| format!("v_{}", id)).collect();
-    format!(
-        "#lang rosette
-         (define (I b) (if b 1 0))
-         (define-symbolic {fvs} integer?)
-         (if (unsat? (verify (assert (eq? {lhs} {rhs}))))
-             (display \"true\")
-             (display \"false\"))",
-        fvs = &fvs_s.join(" "),
-        lhs = lhs,
-        rhs = rhs
-    )
+pub fn lemmas() -> Vec<Rewrite<Semiring, SemiringAnalysis>> {
+    vec![
+        rw!("l-49";  "(I (< ?j ?t))" <=> "(+ (I (< ?j (- ?t 1))) (I (= ?j (- ?t 1))))"),
+        // rw!("l-50";  "0" <=> "(* (I (< ?j ?s)) (I (= ?j ?s)))"),
+        rw!("l-51";  "(I (< ?j ?t))" <=> "(* (I (< ?j ?t)) (I (<= ?j ?t)))"),
+        rw!("l-52";  "(I (= ?j ?t))" <=> "(* (I (= ?j ?t)) (I (<= ?j ?t)))"),
+        rw!("l-53";  "(I (< ?j ?t))" <=> "(I (<= ?j (- ?t 1)))"),
+    ]
+    .concat()
 }
