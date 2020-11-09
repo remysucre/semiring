1bf48def19361665a59f5e896d6b0f475a9cbcf8
Remy Wang <remy.sucre@gmail.com>
2020-11-09 11:39:45 -0800

    Verify running total.

--- tests/examples.rs
+++ tests/examples.rs
@@ -1,11 +1,19 @@
 use egg::*;
 use semiring::rewrites::*;
 
+fn check_eq(x: &str, y:&str) {
+    let runner = Runner::default()
+        .with_expr(&x.parse().unwrap())
+        .with_expr(&y.parse().unwrap())
+        .run(&rules());
+    let lhs = runner.roots[0];
+    let rhs = runner.roots[1];
+    assert_eq!(runner.egraph.find(lhs), runner.egraph.find(rhs))
+}
+
 #[test]
 fn apsp() {
-    let runner = Runner::default()
-        .with_expr(
-            &"
+    check_eq("
 (sum (var w)
      (* (+ (rel E (var x) (var z) (var w))
            (sum (var y)
@@ -14,12 +22,8 @@ fn apsp() {
                           (* (* (rel R (var x) (var y) (var w1))
                                 (rel E (var y) (var z) (var w2)))
                              (I (= (var w) (* (var w1) (var w2)))))))))
-        (var w)))"
-                .parse()
-                .unwrap(),
-        )
-        .with_expr(
-            &"
+        (var w)))
+", "
 (+ (sum (var w)
         (* (var w)
            (rel E (var x) (var z) (var w))))
@@ -30,11 +34,34 @@ fn apsp() {
            (sum (var w2)
                 (* (var w2)
                    (rel E (var y) (var z) (var w2)))))))"
-                .parse()
-                .unwrap(),
-        )
-        .run(&rules());
-    let lhs = runner.roots[0];
-    let rhs = runner.roots[1];
-    assert_eq!(runner.egraph.find(lhs), runner.egraph.find(rhs))
+    )
+}
+
+#[test]
+fn running_total() {
+    check_eq("
+(sum (var w)
+     (sum (var j)
+          (* (* (var w) (* (I (<= 1 (var j)))
+                           (I (<= (var j) (var t)))))
+             (+ (* (I (= (var t) (var j)))
+                   (rel v (var j) (var w)))
+                (* (rel R (- (var t) 1) (var j) (var w))
+                   (* (I (< (var j) (var t)))
+                      (I (> (var t) 1))))))))
+", "
+(+ (* (I (> (var t) 1))
+      (sum (var j)
+           (sum (var w)
+                (* (* (rel R (- (var t) 1) (var j) (var w))
+                      (var w))
+                   (* (I (<= (var j) (- (var t) 1)))
+                      (I (<= 1 (var j))))))))
+   (sum (var j)
+        (sum (var w)
+             (* (* (rel v (var j) (var w))
+                   (var w))
+                (* (I (= (var t) (var j)))
+                   (I (<= 1 (var j))))))))"
+    )
 }
