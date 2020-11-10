affcb7768de44d96e109c8d5f366dd85056e50f0
Remy Wang <remy.sucre@gmail.com>
2020-11-09 23:08:27 -0800

    tweak.

--- tests/examples.rs
+++ tests/examples.rs
@@ -281,5 +281,8 @@ fn doesnt_work() {
 
 #[test]
 fn test_stupid() {
-    check_eq("(- (* (var a) (var c)) (* (var b) (var c)))", "(* (var c) (- (var a) (var b)))")
+    check_eq(
+        "(- (* (var a) (var c)) (* (var b) (var c)))",
+        "(* (var c) (- (var a) (var b)))"
+    )
 }
